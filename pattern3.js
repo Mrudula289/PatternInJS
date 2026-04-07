d6d17334d5daf8413c45ceb46d2fcc6b6692ef95
Mrudula289 <mrudulashastre29@gmail.com>
2026-04-07 15:22:54 +0530

    updated

--- pattern3.js
+++ pattern3.js
@@ -14,4 +14,21 @@ for (let i = 1; i <= n; i++) {
 }
 printPyramid(5);
 
+function printPyramid1(n) {
+
+for (let i = 1; i <= n; i++) {
+    let line = " ";
+    for (let j = 1; j <= n - i; j++) {
+        line = line+" ";
+    }
+    for (let k = 1; k <=i*2-1; k++) {
+        line = line + "  *";
+    }           
+    console.log(line);
+}
+
+}
+printPyramid1(5);
+
+
 
